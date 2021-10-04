c3ea707c79c9ffbadc83e7870272f78bf303c808
JacobVaerst23 <816769@mystma.org>
2021-10-04 09:39:18 -0500

    hasL attempt 1

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 #WRITE YOUR CODE IN THIS FILE
+
+
+
+def hasL(w):
+    
+    for i in range(0, len(w)):
+        
+
+        if (w[i]) == "l":
+             return True
+
+    else:
+            return False
+
+
+print(hasL("alabama"))
+
